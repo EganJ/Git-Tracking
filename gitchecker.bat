54382e5788f4c43665e9ea2d4e9c2bbf61e3bb1b
EganJ <EganJ@users.noreply.github.com>
2019-06-30 10:09:20 -0500

    Check All Status

--- gitchecker.bat
+++ gitchecker.bat
@@ -1,2 +1,8 @@
-@echo off
 rem A script for running checkstatus for each git repo in C:\GitRepositories
+set startingdir = %cd%
+
+cd C:/GitRepositories
+for /f "delims=" %%g in ('dir /ad /b') do start "%%g" cmd /k ("C:\GitRepositories\Repository Tracking\checkonerepo.bat" "C:\GitRepositories\%%g")
+
+cd %startingdir%
+
